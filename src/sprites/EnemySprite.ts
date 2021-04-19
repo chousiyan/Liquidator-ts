719a0ce53302edfbcad78b10ad776c40f3bb46c6
Ali Karbassi <ali@karbassi.com>
2021-04-19 14:35:27 -0500

    Sprite cleanup

--- src/sprites/EnemySprite.ts
+++ src/sprites/EnemySprite.ts
@@ -1,45 +1,52 @@
 import Phaser from 'phaser';
+import PlayerSprite from './PlayerSprite';
 
 export default class EnemySprite extends Phaser.Physics.Arcade.Sprite {
   // Default speed = 100
   speed: number = 130;
+  player: PlayerSprite;
 
   constructor(scene: Phaser.Scene, x, y, key) {
     super(scene, x, y, key);
+
+    this.player = this.scene.player;
 
     scene.add.existing(this);
     scene.physics.add.existing(this);
   }
 
-  facingDirection(player, frontKey, backKey) {
+  preUpdate(time: number, delta: number) {
+    super.preUpdate(time, delta);
+
+    this.scene.physics.moveToObject(this, this.player, 100);
+    this.facePlayer();
+  }
+
+  facePlayer() {
     // facing back
-    if (this.y > player.y) {
-      if (this.x <= player.x) {
+    if (this.y > this.player.y) {
+      if (this.x <= this.player.x) {
         // back right
-        this.setTexture(backKey);
+        this.setTexture('rabbit-back');
         this.flipX = true;
       } else {
         // back left
-        this.setTexture(backKey);
+        this.setTexture('rabbit-back');
         this.flipX = false;
       }
     }
 
     // facing front
-    if (this.y <= player.y) {
-      if (this.x <= player.x) {
+    if (this.y <= this.player.y) {
+      if (this.x <= this.player.x) {
         // front right
-        this.setTexture(frontKey);
+        this.setTexture('rabbit');
         this.flipX = true;
       } else {
         // front left
-        this.setTexture(frontKey);
+        this.setTexture('rabbit');
         this.flipX = false;
       }
     }
-  }
-
-  movement(player) {
-    this.scene.physics.moveToObject(this, player, this.speed);
   }
 }
